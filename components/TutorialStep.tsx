f20b42e3f44e8e13c66087843cec51b8d4c45804
dsaad12344321-svg <dsaad12344321@gmail.com>
2026-07-27 01:29:04 +0300

    Update TutorialStep.tsx

--- components/TutorialStep.tsx
+++ components/TutorialStep.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type Props = {
   image: string;
   text: string;
@@ -8,10 +10,22 @@ export default function TutorialStep({
   text,
 }: Props) {
   return (
-    <section>
-      {/* Image */}
+    <section className="mb-10">
 
-      {/* Text */}
+      <div className="overflow-hidden rounded-xl border bg-white shadow-sm">
+        <Image
+          src={image}
+          alt={text}
+          width={1200}
+          height={700}
+          className="w-full h-auto"
+        />
+      </div>
+
+      <p className="mt-4 text-lg leading-8 text-gray-700">
+        {text}
+      </p>
+
     </section>
   );
 }
