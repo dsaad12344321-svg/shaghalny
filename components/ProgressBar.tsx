37ce53bfc03f49b8238191bc45d5171ac98d82a4
dsaad12344321-svg <dsaad12344321@gmail.com>
2026-07-27 01:19:47 +0300

    Update ProgressBar.tsx

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -7,33 +7,9 @@ export default function ProgressBar({
   current,
   total,
 }: Props) {
-
-  const percentage = (current / total) * 100;
-
   return (
-    <div className="mb-8">
-
-      <div className="mb-2 flex justify-between text-sm">
-        <span>
-          الخطوة {current}
-        </span>
-
-        <span>
-          {total}
-        </span>
-      </div>
-
-      <div className="h-3 rounded-full bg-gray-200">
-
-        <div
-          className="h-3 rounded-full bg-green-600"
-          style={{
-            width: `${percentage}%`,
-          }}
-        />
-
-      </div>
-
+    <div>
+      {/* Progress */}
     </div>
   );
 }
