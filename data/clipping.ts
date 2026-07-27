7cee7925c6ebee7102e024ccf80c945e33ebf90a
dsaad12344321-svg <dsaad12344321@gmail.com>
2026-07-28 00:53:08 +0300

    Update clipping.ts

--- data/clipping.ts
+++ data/clipping.ts
@@ -17,7 +17,7 @@ export const clippingLessons: Lesson[] = [
     description: "تعلم كيفية إنشاء قناة TikTok خطوة بخطوة.",
     steps: [
       {
-        image: "../cr1.jpg",
+        image: "/cr1.jpg",
         text: "افتح تطبيق TikTok واضغط على تسجيل حساب جديد.",
       },
       {
