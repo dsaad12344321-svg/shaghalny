766151e8e87fb865376cb07c2683e6f03822458d
dsaad12344321-svg <dsaad12344321@gmail.com>
2026-08-05 23:47:19 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ar" dir="rtl">
+     <html lang="ar" dir="rtl">
       <body className="min-h-screen bg-slate-50 text-slate-900 antialiased">
         {children}
+
+        <Script
+          src="https://pl30689020.effectivecpmnetwork.com/c5/0c/3e/c50c3e6f2d075d4a31494a207d598f08.js"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
